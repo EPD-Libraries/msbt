3bca7b65cddfc03e900687ab60b86afb0ad7b7bc
ArchLeaders <archparliament@gmail.com>
2023-05-20 17:29:10 -0700

    Update test program

--- tests/src/include/utils/file_util.h
+++ tests/src/include/utils/file_util.h
@@ -8,13 +8,27 @@
 
 namespace file::util {
 
-std::vector<u8> ReadAllBytes(const char* file_path) {
+std::vector<u8> read_all_bytes(std::string& file_path) {
   std::vector<u8> file;
   std::ifstream stream{file_path, std::ios::binary | std::ios::ate};
   file.resize(stream.tellg());
   stream.seekg(0, std::ios::beg);
   stream.read(reinterpret_cast<char*>(file.data()), file.size());
   return std::move(file);
+}
+
+size_t get_extension_index(std::string& path) {
+  size_t pos = 0;
+  size_t index = 0;
+  while ((pos = path.find('.', pos)) != std::string::npos) {
+    index = pos++;
+  }
+
+  return index;
+}
+
+std::optional<std::string> get_extension(std::string& path) {
+  return path.substr(get_extension_index(path), path.length());
 }
 
 }  // namespace file::util
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,16 +1,29 @@
-#include <codecvt>
 #include <iostream>
-#include <locale>
 #include <msbt/msbt.h>
-#include <string>
 
 #include "utils/file_util.h"
 
 int main(int argc, char** argv) {
-  std::cout << "[c++] Init Testing" << std::endl;
-  const auto file = file::util::ReadAllBytes(argv[1]);
-  auto msbt = oepd::msbt::FromBinary(file);
+  std::string path{argv[1]};
+  std::string ext = *file::util::get_extension(path);
 
-  std::ofstream stream(std::string{argv[1]} + ".yml", std::ios::binary);
-  stream << msbt.ToText();
+  if (ext == ".yaml" || ext == ".yml") {
+    const auto file = file::util::read_all_bytes(path);
+    auto msbt = oepd::msbt::FromText(reinterpret_cast<const char*>(file.data()));
+
+    size_t idx = file::util::get_extension_index(path);
+    std::string outfile = path.substr(0, idx) + ".msbt";
+
+    std::ofstream stream(outfile, std::ios::binary);
+    stream << msbt.ToBinary().data();
+  } else if (ext == ".msbt") {
+    const auto file = file::util::read_all_bytes(path);
+    auto msbt = oepd::msbt::FromBinary(file);
+
+    size_t idx = file::util::get_extension_index(path);
+    std::string outfile = path.substr(0, idx) + ".yml";
+
+    std::ofstream stream(outfile, std::ios::binary);
+    stream << msbt.ToText();
+  }
 }
