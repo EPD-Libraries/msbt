4d981a4b37e92e02a2d934880f2e9f5e93ade9a6
ArchLeaders <archleadership28@gmail.com>
2023-10-26 12:07:06 -0700

    Update font_size tag impl

--- src/tags/font_size_tag.cpp
+++ src/tags/font_size_tag.cpp
@@ -8,15 +8,15 @@ void FontSizeTag::Fill(u16 group_id, u16 type_id, tcb::span<const u8> data) {
 }
 
 void FontSizeTag::Fill(std::string_view group_name, TagParams params) {
-  for (const auto entry : params) {
-    if (entry.first == "Value") {
+  for (const auto& entry : params) {
+    if (entry.first == "Scale") {
       std::from_chars(entry.second.data(), entry.second.data() + entry.second.size(), m_font_size);
     }
   }
 }
 
 std::string FontSizeTag::ToText() {
-  return "<FontSize Value='" + std::to_string(m_font_size) + "'/>";
+  return "<FontSize Scale='" + std::to_string(m_font_size) + "'/>";
 }
 
 void FontSizeTag::ToBinary(exio::BinaryWriter& writer) {
