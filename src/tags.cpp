813f591aaeec4907e295759ed3c9b1d4778cee26
ArchLeaders <archleadership28@gmail.com>
2023-10-26 12:09:10 -0700

    Update tag processor module

--- src/tags.cpp
+++ src/tags.cpp
@@ -1,14 +1,33 @@
 #include "msbt/tags.h"
 #include "util.h"
 
+
 namespace oepd::msbt::tags {
+  
+#define TagCase(a, b) a - (b * b)
 
 Tag* CreateTag(u16 group_id, u16 type_id, tcb::span<const u8> data) {
   Tag* tag;
-  if (group_id == 0 && type_id == 2) {
-    tag = new FontSizeTag;
-  } else {
-    tag = new UnknownTag;
+  s16 match = group_id - (type_id * type_id);
+  switch (match) {
+    case TagCase(0, 0):
+      tag = new FontTag;
+      break;
+    case TagCase(0, 2):
+      tag = new FontSizeTag;
+      break;
+    case TagCase(2, 1):
+      tag = new StringFlagTag;
+      break;
+    case TagCase(2, 2):
+      tag = new IntegerFlagTag;
+      break;
+    case TagCase(2, 9):
+      tag = new FloatFlagTag;
+      break;
+    default:
+      tag = new UnknownTag;
+      break;
   }
 
   tag->Fill(group_id, type_id, data);
@@ -21,6 +40,14 @@ Tag* CreateTag(std::string_view text) {
 
   if (tag_info.first == "FontSize") {
     tag = new FontSizeTag;
+  } else if (tag_info.first == "Font") {
+    tag = new FontTag;
+  } else if (tag_info.first == "IntFlag") {
+    tag = new IntegerFlagTag;
+  } else if (tag_info.first == "StringFlag") {
+    tag = new StringFlagTag;
+  } else if (tag_info.first == "FloatFlag") {
+    tag = new FloatFlagTag;
   } else {
     tag = new UnknownTag;
   }
